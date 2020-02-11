0535815300e2ec3e3b57ca996d5575ec090de887
muranet+git@gmail.com <muranet+git@gmail.com>
2020-02-11 16:32:16 +0900

    DB接続部分をheroku向けに変更

--- db/init.go
+++ db/init.go
@@ -13,19 +13,31 @@ var (
 	err error
 )
 
+// Init ..
 func Init() {
-	fmt.Println("DB_USERNAME = " + os.Getenv("EDITORIAL_DB_USERNAME"))
-	db, err = gorm.Open("postgres",
-		"user="+os.Getenv("EDITORIAL_DB_USERNAME")+" password="+os.Getenv("EDITORIAL_DB_PASSWORD")+" dbname=editorial sslmode=disable")
+	//fmt.Println("DB_USERNAME = " + os.Getenv("EDITORIAL_DB_USERNAME"))
+	// db, err = gorm.Open("postgres",
+	// 	"user="+os.Getenv("EDITORIAL_DB_USERNAME")+" password="+os.Getenv("EDITORIAL_DB_PASSWORD")+" dbname=editorial sslmode=disable")
+
+	// heroku接続用
+	url := os.Getenv("DATABASE_URL")
+	connection, err := pq.ParseURL(url)
+	if err != nil {
+		panic(err.Error())
+	}
+	connection += " sslmode=require"
+	db, err := gorm.Open("postgres", connection)
 	if err != nil {
 		panic(err)
 	}
 }
 
+// GetDb ..
 func GetDb() *gorm.DB {
 	return db
 }
 
+// Close ..
 func Close() {
 	if err := db.Close(); err != nil {
 		panic(err)
